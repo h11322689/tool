914b6fb7f6a300474eb92edd74316a4fa4d6ed57
h11322689 <h11322689@gmail.com>
2024-07-18 22:23:49 +0800

    fix clear_cache.sh

--- clear_cache.sh
+++ clear_cache.sh
@@ -7,26 +7,26 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 # 清理 APT 缓存
-sudo apt-get clean
+sudo apt  clean
 
 # 自动清理无用的依赖项
-sudo apt-get autoremove
+sudo apt  autoremove
 
 # 清理临时文件
-sudo rm -rf /tmp/*
+#sudo rm -rf /tmp/*
 
 # 清理用户缓存
-rm -rf ~/.cache/*
+#rm -rf ~/.cache/*
 
 # 清理日志文件（需谨慎操作）
-sudo find /var/log -name "*.gz" -type f -mtime +7 -delete  # 仅删除 7 天前的.gz 日志文件
+#sudo find /var/log -name "*.gz" -type f -mtime +7 -delete  # 仅删除 7 天前的.gz 日志文件
 
 # 清理 DNS 缓存（如果安装了 nscd）
-if dpkg -l | grep -q nscd; then
-    sudo /etc/init.d/nscd restart
-else
-    echo "nscd 未安装，跳过 DNS 缓存清理"
-fi
+#if dpkg -l | grep -q nscd; then
+#    sudo /etc/init.d/nscd restart
+#else
+#    echo "nscd 未安装，跳过 DNS 缓存清理"
+#fi
 
 # 清除 PageCache、dentries 和 inodes 缓存
 sudo sync
